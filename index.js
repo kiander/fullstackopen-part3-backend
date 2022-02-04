5594883cd54431ee699120c7b9fdbf84d6a57945
Otto Kiander <otto.kiander@tuni.fi>
2022-02-04 12:59:28 +0200

    uibuild

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(
 
 app.use(express.static('build'))
 
-app.get('/info', (req, res) =>
+app.get('/info', (req, res, next) =>
 {
     const date = new Date()
 
@@ -36,54 +36,38 @@ app.get('/info', (req, res) =>
     {
         res.send(`<p>${date}</p><p>Phonebook has info for ${persons.length} people</p>`)
     })
+    .catch(error => next(error))
 
 })
 
-app.get('/api/persons', (req, res) =>
+app.get('/api/persons', (req, res, next) =>
 {
     Person.find({}).then(persons =>
     {
         res.json(persons)
     })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) =>
+app.post('/api/persons', (req, res, next) =>
 {
     const body = req.body
-
-    if (!body.name)
-    {
-        return res.status(400).json({
-            error: 'name missing'
-        })
-    }
-
-    if (!body.number)
-    {
-        return res.status(400).json({
-            error: 'number missing'
-        })
-    }
-
-    // if (persons.find(person => person.name === body.name)) {
-    //     return res.status(400).json({
-    //         error: 'name already in list'
-    //     })
-    // }
 
     const person = new Person({
         name: body.name,
         number: body.number,
     })
 
-    person.save().then(savedPerson =>
-    {
-        res.json(savedPerson)
-    })
+    person
+        .save().then(savedPerson =>
+        {
+            res.json(savedPerson)
+        })
+        .catch(error => next(error))
 
 })
 
-app.get('/api/persons/:id', (req, res) =>
+app.get('/api/persons/:id', (req, res, next) =>
 {
     Person.findById(req.params.id)
         .then(person =>
@@ -123,6 +107,10 @@ const errorHandler = (error, request, response, next) =>
     if (error.name === 'CastError')
     {
         return response.status(400).send({ error: 'malformatted id' })
+    }
+    else if (error.name === 'ValidationError')
+    {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
